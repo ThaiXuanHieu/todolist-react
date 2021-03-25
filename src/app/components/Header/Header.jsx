f61231dece9c310bc104c21a3e6137893382abc1
ThaiXuanHieu <s2freedom6789@gmail.com>
2021-03-25 11:11:50 +0700

    update search task

--- src/app/components/Header/Header.jsx
+++ src/app/components/Header/Header.jsx
@@ -1,12 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import { Redirect } from "react-router";
 import { useDispatch } from "react-redux";
 import { logout } from "../../actions/userAction";
 import "./style.css";
 import { Link } from "react-router-dom";
 import Icon from "@material-ui/core/Icon";
+import * as taskAction from "../../actions/taskAction";
 
 const Header = (props) => {
+  const [searchString, setSearchString] = useState("");
+
   const dispatch = useDispatch();
   const user = JSON.parse(localStorage.getItem("user"));
   if (!user) {
@@ -17,19 +20,30 @@ const Header = (props) => {
     dispatch(logout());
   };
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    dispatch(taskAction.search(user.id, searchString));
+  };
+
   return (
     <div className="header d-flex align-items-center justify-content-between">
       <div className="brand-name">
         <h6 className="m-0">To Do</h6>
       </div>
       <div>
-        <form>
+        <form onSubmit={handleSubmit}>
           <div className="form-group m-0">
-          <Icon style={{position:"absolute", margin: "5px", color:"#3e69e4"}}>search</Icon>
+            <Icon
+              style={{ position: "absolute", margin: "5px", color: "#3e69e4" }}
+            >
+              search
+            </Icon>
             <input
               type="text"
               placeholder="Search..."
               className="input-search"
+              value={searchString}
+              onChange={(e) => setSearchString(e.target.value)}
             />
           </div>
         </form>
