1cc2abc11b5a4fd65c87c677fe4a1d5cb3a0d73d
ThaiXuanHieu <s2freedom6789@gmail.com>
2021-03-29 11:46:24 +0700

    update profile

--- src/app/components/Auth/Profile.jsx
+++ src/app/components/Auth/Profile.jsx
@@ -1,9 +1,35 @@
+import Header from "../Header/Header";
+import SideBar from "../SideBar/SideBar";
+
 const Profile = (props) => {
-    return(
-        <div>
-            Profile
+  const user = JSON.parse(localStorage.getItem("user"));
+  const [firstName, ...lastName] = user.fullName.split(" ");
+  return (
+    <div className="task-page">
+      <div className="text-center">
+        <Header />
+      </div>
+      <div className="container-fluid">
+        <div className="row" style={{ height: "100vh" }}>
+          <div className="col-left col-md-2 p-0">
+            <SideBar />
+          </div>
+          <div
+            className="col-center col-md-10"
+            style={{ backgroundColor: "white", height: "100%" }}
+          >
+            <h5 className="m-3" style={{ color: "#3e69e4" }}>
+              Profile
+            </h5>
+            <div className="ml-3">
+              <p>FirstName : {firstName}</p>
+              <p>LastName : {lastName.join(" ")}</p>
+            </div>
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default Profile;
