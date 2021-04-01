7d6d2534519657ecf51c193183ba8d331085b31c
ThaiXuanHieu <s2freedom6789@gmail.com>
2021-04-01 14:39:58 +0700

    update

--- src/app/components/Task/TaskDetail.jsx
+++ src/app/components/Task/TaskDetail.jsx
@@ -5,7 +5,6 @@ import Step from "../Step/Step";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 import "./style.css";
-import "./Task.js";
 import {
   FormatDateInput,
   FormatHour,
